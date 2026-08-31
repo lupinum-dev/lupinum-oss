5066d2d624ce3e33afd745dae60dc8f847a8f59c
Matthias Amon <matthias@lupinum.com>
2026-08-31 16:38:53 +0200

    chore(docs): align the shared Lupinum docs runtime (#48)

--- docs/content.config.ts
+++ docs/content.config.ts
@@ -5,7 +5,8 @@ export default defineGinkoDocsConfig({
   site: {
     name: site.name,
     description: site.description,
-    url: site.url,
+    whenToUse:
+      "Use this site to create, operate, and release Lupinum open-source repositories.",
   },
   locales: ["en"],
   blog: false,
