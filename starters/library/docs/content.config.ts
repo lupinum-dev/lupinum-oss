ab837b2138a66ef6009e21dda214ebae3251851c
Mat4m0 <matthias@lupinum.com>
2026-08-31 16:24:52 +0200

    chore(docs): align the shared Lupinum docs runtime

--- starters/library/docs/content.config.ts
+++ starters/library/docs/content.config.ts
@@ -1,7 +1,11 @@
 import { defineGinkoDocsConfig } from '@lupinum/ginko-docs/content'
 
 export default defineGinkoDocsConfig({
-  site: { name: '{{TITLE}}', description: '{{DESCRIPTION}}', url: 'https://{{DOMAIN}}' },
+  site: {
+    name: '{{TITLE}}',
+    description: '{{DESCRIPTION}}',
+    whenToUse: 'Use this site to learn and operate {{TITLE}}.',
+  },
   locales: ['en'],
   blog: false,
 })
